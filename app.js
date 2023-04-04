39dc40ba4d2084da572579fd0f11e9f25af6a54e
jaffa Isaac <jaffaisaac4@gmail.com>
2023-04-04 23:05:11 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+// IIFE -> immediately Invoked Function Expressions
+// this is a function expression that automatically invoke after conplet of the definition
+// syntax
+
+// (function)()
+
+(function IIFE(){
+    console.log(`IIFE function executed already`)
+})()
